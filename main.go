b25e51d45ea5901da29ea3f0c1409aca228e8047
root <brownjab21@gmail.com>
2022-12-14 05:29:13 +0000

    Mempool

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"log"
-
 	"flexgo/mempool"
+
+    "github.com/ethereum/go-ethereum/rpc"
+    "github.com/ethereum/go-ethereum/ethclient"
 )
 
 func main() {
 	log.Println("Starting mempool listener")
 
-	ml := &mempool.MempoolListener{}
-	ml.Start()
+    rc, err := rpc.Dial("ws://localhost:8546")
+    if err != nil {
+        log.Printf("Failed to dial rpc: %v", err.Error())
+        return
+    }
+
+    c, err := ethclient.Dial("ws://localhost:8546")
+    if err != nil {
+        log.Printf("Failed to dial websocket: %s", err.Error())
+        return
+    }
+
+	ml := &mempool.Mempool{}
+	ml.Start(rc, c)
 }
--- mempool/mempool.go
+++ mempool/mempool.go
@@ -3,10 +3,56 @@ package mempool
 import (
     "log"
     "context"
+
+    "github.com/ethereum/go-ethereum/rpc"
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/ethclient/gethclient"
+    "github.com/ethereum/go-ethereum/common"
 )
 
 type Mempool struct {}
 
-func (m *Mempool) Start() {
+func (m *Mempool) Start(rc *rpc.Client, ec *ethclient.Client) {
+    gc := gethclient.New(rc)
 
+    ch := make(chan common.Hash)
+    _, err := gc.SubscribePendingTransactions(context.Background(), ch)
+    if err != nil {
+        log.Printf("Failed to sub to pending txs: %v", err.Error())
+        return
+    }
+
+    for h := range ch {
+        tx, p, err := ec.TransactionByHash(context.Background(), h)
+        if err != nil {
+            log.Printf("Failed to fetch tx (%s) by hash: %v", h, err.Error())
+        }
+
+        if p != true {
+            log.Printf("Transaaction hash %v is not pending", h)
+            continue
+        }
+
+        h := common.HexToAddress("0xd4a0e3ec2a937e7cca4a192756a8439a8bf4ba91") 
+        if tx.To() == &h {
+
+        log.Printf("%s\x1b[31m%s\x1b[0m%s", "---------- ", "EF Transaction Found", " ----------")
+        log.Printf("Hash: %s", h)
+        log.Printf("To: %s", tx.To())
+        log.Printf("Cost: %s", tx.Cost())
+        log.Printf("Gas Limit: %s", tx.Gas())
+        log.Printf("Nonce: %s", tx.Nonce())
+        log.Printf("Data: %s", tx.Data())
+
+        continue
+    }
+
+        log.Printf("%s\x1b[32m%s\x1b[0m%s", "---------- ", "Transaction Found", " ----------")
+        log.Printf("Hash: %s", h)
+        log.Printf("To: %s", tx.To())
+        log.Printf("Cost: %s", tx.Cost())
+        log.Printf("Gas Limit: %s", tx.Gas())
+        log.Printf("Nonce: %s", tx.Nonce())
+        log.Printf("Data: %s", tx.Data())
+    } 
 }
