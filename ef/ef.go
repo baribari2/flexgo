fbb6e7626f4621f0026056b35b74874a8e0ef7f1
root <brownjab21@gmail.com>
2022-12-14 07:20:33 +0000

    More

--- ef/ef.go
+++ ef/ef.go
@@ -2,12 +2,52 @@ package ef
 
 import (
     "log"
-    "flexgo/abi"
+    fga "flexgo/abi"
+    "sync"
+
+    "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/core/types"
 )
 
 type EF struct {}
 
-func (e *EF) Start() {
-    
+func (e *EF) Start(wg *sync.WaitGroup, ec *ethclient.Client) {
+    defer wg.Done()
+
+
+    pk, err := crypto.HexToECDSA(PK)
+    if err != nil {
+        log.Printf("Failed to creake private key: %v", err.Error())
+        return
+    }
+
+    pubK := pk.Public()
+    pke, ok := pubK.(ecdsa.*publicKeyECDSA)
+    if !ok {
+        log.Printf("Failed to create public key: %v", err.Error())
+        return 
+    }
+
+    auth := bind.NewKeyedTransactor(pk)
+
+
+    exf, err := fga.NewExternallyFunded(common.HexToAddress("0xD4A0E3EC2A937E7CCa4A192756a8439A8BF4bA91"), auth)
+    if err != nil {
+        log.Printf("Failed to make EF instance: %v", err.Error())
+        return
+    }
+
+    ch := make(chan *types.Header)
+
+    _, err := ec.SubscribeNewHead(context.Background(), ch)
+    if err != nil {
+        log.Printf("Failed tp sub to new head: %v", err.Error()
+        return
+    }
+
+    log.Printf("EF: %v", exf)
+
+    // for h := ch {
+
 }
 
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"flexgo/mempool"
+    fga "flexgo/abi"
 
     "github.com/ethereum/go-ethereum/rpc"
     "github.com/ethereum/go-ethereum/ethclient"
+    "github.com/ethereum/go-ethereum/accounts/abi/bind"
 )
 
 func main() {
@@ -25,4 +27,21 @@ func main() {
 
 	ml := &mempool.Mempool{}
 	ml.Start(rc, c)
+
+    pk, err := crypto.HexToECDSA(PK)
+    if err != nil {
+        log.Printf("Failed to creake private key: %v", err.Error())
+        return
+    }
+
+    pubK := pk.Public()
+    pke, ok := pubK.(ecdsa.*publicKeyECDSA)
+    if !ok {
+        log.Printf("Failed to create public key: %v", err.Error())
+        return 
+    }
+
+    auth := bind.NewKeyedTransactor(pk)
+
+    fga.NewExternallyFunded(common.HexToAddress("0xD4A0E3EC2A937E7CCa4A192756a8439A8BF4bA91"), auth)
 }
